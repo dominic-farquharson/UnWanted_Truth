893f68c3a5c5f86b99c964a53423245c7186e517
Dominic Farquharson <farquharsondominic@gmail.com>
2017-06-14 19:54:51 -0400

    removing axios

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Footer from '../Footer';
 import $ from 'jquery';
-import axios from 'axios';
 
 class Contact extends Component {
     constructor() {
